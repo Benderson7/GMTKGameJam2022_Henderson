4684de5b898a75408fcc424d950e89735580ad50
Benderson7 <ben.henderson.bth@gmail.com>
2022-07-08 18:25:43 -0400

    omg the game is almost here

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -7,24 +7,20 @@ public class GameplayManager : MonoBehaviour
     public ScoreboardManager scoreboardManager;
     public SongManager songManager;
     public TimeClock timeClock;
-    public NoteBlock noteBlock; // Temp
+    public AudioSource song;
     void Update()
     {
         if (Input.anyKeyDown) {
             if (timeClock.started)
             {
                 double curTime = timeClock.timeElapsed.TotalSeconds;
-                Note[] notes = songManager.GetCurrentNotes(curTime);
+                Note[] notes = songManager.GetNotes(note => songManager.CloseEnough(note.time, curTime));
                 ActionManager.Action hit = ActionManager.Hit(notes);
                 scoreboardManager.UpdateScoreboard(hit);
             } else if (Input.GetKey(KeyCode.Space))
             {
                 timeClock.StartClock();
-            }
-            if (Input.GetKeyDown(KeyCode.P))
-            {
-                Debug.Log("Note Block Created.");
-                Instantiate(noteBlock, new Vector2(0, 0), Quaternion.identity);
+                song.Play();
             }
         }
     }
--- Assets/Scripts/SongManager.cs
+++ Assets/Scripts/SongManager.cs
@@ -1,25 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SongManager : MonoBehaviour
 {
     public TextAsset jsonFile;
     public double halfBufferRange;
+    public double doublePrecision;
     private List<Note> notes;
-    public Note[] GetCurrentNotes(double time)
+    public NoteBlock noteBlock;
+    public TimeClock timeClock;
+    public double preHitSpawnTime;
+
+    void Update()
     {
-        notes = notes ?? new List<Note>(NoteMap.CreateFromJSON(jsonFile.text).notes);
-        return notes.FindAll(note => CloseEnough(note.time, time)).ToArray();
+        if (timeClock.started)
+        {
+            foreach (Note note in GetNotes(note => AlmostEqual(note.time, timeClock.timeElapsed.TotalSeconds - preHitSpawnTime)))
+            {
+                if (note.midi == 64)
+                {
+                    Instantiate(noteBlock, new Vector2(-9.5f, 3.5f), Quaternion.identity);
+                }
+                else if (note.midi == 69)
+                {
+                    Instantiate(noteBlock, new Vector2(9.5f, 3.5f), Quaternion.identity);
+                }
+            }
+        }
     }
 
-    private bool CloseEnough(double noteTime, double time)
+    public bool CloseEnough(double noteTime, double time)
     {
         bool closeEnough = time < noteTime + halfBufferRange && time > noteTime - halfBufferRange;
         if (closeEnough)
         {
-            Debug.Log($"Note's Time Value: {noteTime}\n Key Pressed At: {time}");  
+            // Debug.Log($"Note's Time Value: {noteTime}\n Key Pressed At: {time}");  
         }
         return closeEnough;
+    }
+
+    public Note[] GetNotes(Predicate<Note> predicate)
+    {
+        notes = notes ?? new List<Note>(NoteMap.CreateFromJSON(jsonFile.text).notes);
+        return notes.FindAll(predicate).ToArray();
+    }
+
+    private bool AlmostEqual(double value1, double value2)
+    {
+        return Math.Abs(value1 - value2) < doublePrecision; 
     }
 }
