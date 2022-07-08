3f13e9732725916dc6d31433cc4310271f47dfa3
Ben Henderson <44758557+Benderson7@users.noreply.github.com>
2022-07-08 13:17:19 -0400

    JSON conversion and a test file (#5)

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Note
+{
+    public int midi;
+    public float time;
+    
+    public override string ToString() => $"<Note: midi({midi}), time({time})>";
+}
